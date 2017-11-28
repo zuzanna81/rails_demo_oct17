cc705be9751f5e0d1c5aab63359ea169b3f8f5b4
Raoul DIFFOUO <diraulo@gmail.com>
2017-11-28 12:51:30 +0200

    Refactor create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,16 +4,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    article = Article.new
-    article.title = params[:article][:title]
-    article.content = params[:article][:content]
-    article.save
-    flash[:notice] = "Article was successfully created."
+    article = Article.create(article_params)
+    flash[:notice] = 'Article was successfully created.'
     redirect_to article
   end
 
   # GET /articles/:id
   def show
     @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params[:article].permit(:title, :content)
   end
 end
