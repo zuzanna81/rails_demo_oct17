081680bdf744f3105bf56eda992850031d53f7e4
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-12-04 13:46:11 +0100

    Save and show comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,16 @@
 class CommentsController < ApplicationController
   def create
+    @article = Article.find(params[:article_id])
+    comment = @article.comments.new(comments_params)
+    if comment.save
+      flash[:notice] = "Comment was successfully created."
+      redirect_to article_path(@article)
+    end
+  end
+
+  private
+
+  def comments_params
+    params[:comment].permit(:body)
   end
 end
